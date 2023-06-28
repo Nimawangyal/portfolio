f35118e41c53b863b80d284835d7b636b65dbe71
nimawangyal <nimawangyalb@gmail.com>
2023-06-28 15:01:13 +0530

    added firebase config

---
+++ firebase/config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABJXDGFuuRltb5d3iSbmrmpbLr7db6L18",
+  authDomain: "portfolio-96e6f.firebaseapp.com",
+  projectId: "portfolio-96e6f",
+  storageBucket: "portfolio-96e6f.appspot.com",
+  messagingSenderId: "544874397520",
+  appId: "1:544874397520:web:7a2d0cd5ae823f68a6f655"
+};
